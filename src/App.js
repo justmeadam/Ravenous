891abed98045f087b990251a29257446ba9496b8
Adam <finn.adam@gmail.com>
2018-06-21 16:58:36 -0700

    Part 1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import BusinessList from './components/BusinessList/';
+import Business from './components/Business/';
+import SearchBar from './components/SearchBar/';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
